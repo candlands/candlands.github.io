c09d0aec75d71960f768786dcc42152ae00b4133
candlands <samuel.candland@svu.edu>
2025-05-03 17:53:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,122 @@
+html {
+  height: 100%;
+  width: 100%;
+}
+body {
+  height: 100%;
+  width: 100%;
+  margin: 0;
+}
+
+.wholepage {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  width: 100%;
+  margin: 0%;
+  background-image: linear-gradient(to top, #393939, #4d4d4d);
+}
+.border2 {
+  border: 2px solid green;
+}
+.border {
+  border: 2px solid;
+}
+.hide {
+  display: none;
+}
+.show {
+  display: block;
+}
+
+.holddiv {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+}
+
+.buttonhold {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 5px;
+}
+.buttonsplit {
+  flex: 1;
+}
+
+.pagehold {
+  flex: 3;
+  height: 100%;
+  width: 100%;
+}
+.page {
+  bpadding-top: 10px;
+  height: 100%;
+  width: 100%;
+  position: relative;
+}
+
+.button1 {
+}
+.introduction {
+  font-size: 40px;
+  color: lightgrey;
+  width: 90%;
+  margin: auto;
+  text-align: center;
+}
+.rosterpic {
+  bposition: absolute;
+  bheight: 40%;
+  display: block;
+  margin: 20px auto;
+  max-height: 70vh;
+  max-width: 80vw;
+  height: auto;
+  width: auto;
+}
+.rosterpicdivb {
+  width: 100%;
+  text-align: center;
+}
+
+.favfood {
+  position: absolute;
+  color: orange;
+  font-size: 20px;
+  font-weight: bold;
+  background-color: brown;
+  padding: 5px;
+}
+.fooditems {
+  color: white;
+  font-size: 15px;
+  font-weight: normal;
+}
+.foodpic {
+  position: absolute;
+  bottom: 0;
+  right: 40px;
+  max-height: 50vh;
+  max-width: 60vw;
+  height: auto;
+  width: auto;
+}
+.theorginpic {
+  max-height: 70vh;
+  max-width: 90vw;
+  height: auto;
+  width: auto;
+}
+.bigtext {
+  text-indent: 25px;
+  font-size: 40px;
+  font-weight: bold;
+}
+.volleyballtext {
+  font-size: 25px;
+  color: black;
+}
 
